60203a33c968f65109e28b7fa884b18066ccf521
Raquel <raquellopezsancheez@gmail.com>
2024-06-17 04:58:30 +0200

    17

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
+//MENÚ HAMBURGUESA
+
 const hamburguerMenu = document.querySelector(".hamburguesa_png");
 
 hamburguerMenu.onclick = function () {
   const navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
+
+//FLECHA TO TOP
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 150) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
