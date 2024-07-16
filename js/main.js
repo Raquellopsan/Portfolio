92ecd783822a25d493a677fa1ceaf9fe7d2aa492
Raquel <raquellopezsancheez@gmail.com>
2024-07-17 01:19:52 +0200

    17

--- js/main.js
+++ js/main.js
@@ -10,9 +10,14 @@ hamburguerMenu.onclick = function () {
 //FLECHA TO TOP
 
 const toTop = document.querySelector(".to-top");
+const footer = document.querySelector("footer");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 150) {
+  const scrollPosition = window.scrollY;
+  const footerPosition = footer.offsetTop;
+  const windowHeight = window.innerHeight;
+
+  if (window.scrollY > 150 && scrollPosition + windowHeight < footerPosition) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
@@ -31,6 +36,23 @@ setTimeout(() => {
 
 const categorias = document.querySelector(".categorias");
 const proyectos = document.querySelectorAll(".card_proyecto");
+
+categorias.addEventListener("click", (event) => {
+  if (event.target.classList.contains("filter-items")) {
+    categorias.querySelector(".active").classList.remove("active");
+    event.target.classList.add("active");
+    const filterValue = event.target.getAttribute("data-filter");
+    proyectos.forEach((item) => {
+      if (item.classList.contains(filterValue) || filterValue == "all") {
+        item.classList.remove("hide");
+        item.classList.add("show");
+      } else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+    });
+  }
+});
 
 // SECCION.offsetTop
 // window.scrollY >= SECCION.offsetTop
